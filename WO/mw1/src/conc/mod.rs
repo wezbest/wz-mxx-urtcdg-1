6f4fb37b783864a600ac23113b71bbf8f076b3d1
wezbest <110793713+wezbest@users.noreply.github.com>
2025-04-28 12:21:47 +0000

    🫦

---
+++ WO/mw1/src/conc/mod.rs
@@ -0,0 +1,7 @@
+/*
+This directory will have files where you learn important comcepts
+in code form
+
+own25 - Chapter 24 - Ownership in rust
+
+*/
---
+++ WO/mw1/src/conc/own24.rs
@@ -0,0 +1,11 @@
+/*
+Chapter 24 - using the same bank example to learn the owbership
+and borrowing concepts in rust
+*/
+
+use crate::utils::{header, pswg};
+use yansi::Paint;
+
+pub fn conc_main() {
+    header("")
+}
