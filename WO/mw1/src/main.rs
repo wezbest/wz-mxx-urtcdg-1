79aa1687daca8e10eb4db2416687217eb14a6067
wezbest <110793713+wezbest@users.noreply.github.com>
2025-04-28 18:13:57 +0000

    🫦

--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -2,13 +2,14 @@
 Main entry point for the Rust application.
 */
 
-// mod conc;
+mod conc;
 mod sec3;
 mod utils;
 
-use sec3::bank1::bank1_main;
+// use sec3::bank1::bank1_main;
 // use conc::exc26::exc26_main;
+use conc::own24::conc_main;
 
 fn main() {
-    bank1_main();
+    conc_main();
 }
