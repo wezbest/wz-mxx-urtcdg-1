463bf76f21c32a994f8735b1f0a81f5abcff8d36
wezbest <110793713+wezbest@users.noreply.github.com>
2025-04-30 14:14:52 +0000

    🫦

---
+++ WO/mw1/src/conc/bor31.rs
@@ -0,0 +1,13 @@
+/*
+Chapter 31 - Focus on borrowing rules
+You will do all the exercises in this chapter using the same code.
+*/
+
+use crate::utils::{header, pswg};
+use yansi::Paint;
+
+///////////////// Main Function calls//////////////////
+
+pub fn bor31_main() {
+    pswg("Chapter 31 - Borrowing".to_string());
+}
--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -8,8 +8,9 @@ mod utils;
 
 // use sec3::bank1::bank1_main;
 // use conc::exc26::exc26_main;
-use conc::own24::conc_main;
+// use conc::own24::conc_main;
+use conc::bor31::bor31_main;
 
 fn main() {
-    conc_main();
+    bor31_main();
 }
