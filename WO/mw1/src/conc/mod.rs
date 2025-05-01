b4cc698b411f0ba75d1ab82a860900d1f2654745
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-01 14:07:23 +0000

    🫦

--- WO/mw1/src/conc/mod.rs
+++ WO/mw1/src/conc/mod.rs
@@ -13,4 +13,5 @@ exc26 - Chapter 26 - Rust Excercises
 
 // pub mod bor31;
 // pub mod own24;
-pub mod mur35;
+// pub mod mur35;
+pub mod copy28;
--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -12,7 +12,7 @@ mod exc;
 // use conc::own24::conc_main;
 // use conc::bor31::bor31_main;
 // use exc::exc33::exc33_main;
-use exc::exc36::exc36_main;
+// use exc::exc36::exc36_main;
 // use conc::mur35::mur35_main;
 
 fn main() {
