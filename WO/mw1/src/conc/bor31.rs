1dbe61f012dc2cd02588a6216b8f7d9cf96bfd47
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-01 09:46:00 +0000

    🫦

--- WO/mw1/src/conc/bor31.rs
+++ WO/mw1/src/conc/bor31.rs
@@ -10,7 +10,8 @@ use yansi::Paint;
 
 pub fn bor31_main() {
     pswg("Chapter 31 - Borrowing Rules".to_string());
-    br1();
+    // br1();
+    br2();
 }
 
 /////// Sub fuynctions for each exercise //////////
@@ -57,6 +58,46 @@ fn br1() {
     // Making multiple reference to read only
     let account_ref1 = &account;
     let account_ref2 = &account;
+
+    // Printing the account reference directly
+    br1_print_account(&account);
+
+    br1_print_account(account_ref);
+
+    println!("{:#?}", account.cyan());
+
+    let text1 = r"
+Printing out only the specific
+elements of the account struct.
+---
+";
+    println!("{}", text1.green());
+    println!("Account ID: {}", account_ref.id);
+    println!("Account Holder: {}", account_ref.holder);
+    println!("Account Balance: {}", account_ref.balance);
+    println!("Account Reference: {:#?}", account_ref);
+}
+
+// Function 2 - illutrating the move for borrowed references
+
+fn br2_print_account(account: &Account) {
+    println!("{:#?}", account.yellow());
+}
+
+fn br2() {
+    header("F2 - Moving Vaues on Borrowed References");
+
+    let account = Account::new(1, String::from("Func Onner"));
+
+    // Dont need to store the borrowed reference.
+    let account_ref = &account;
+
+    // Making multiple reference to read only
+    let account_ref1 = &account;
+    let account_ref2 = &account;
+
+    // Moving values to the borrowed reference
+    let other_account = account;
 
     // Printing the account reference directly
     br1_print_account(&account);
