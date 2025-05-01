e852bb310826aa2ac4d644aa8e15790089cbf350
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-01 10:01:04 +0000

    🫦

--- WO/mw1/src/exc/exc33.rs
+++ WO/mw1/src/exc/exc33.rs
@@ -9,5 +9,67 @@ use yansi::Paint;
 ////////////// Main Function calls//////////////////
 pub fn exc33_main() {
     pswg("Chapter 33 - Excercises On references".to_string());
-    ex2();
+    ex1();
+}
+
+//// Sub functions here ////
+
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+// TODO: Create a new function called 'print_num_accounts'
+// It should take in a Bank as an argument, then print out the number
+// of accounts stored in the 'bank.accounts' vector
+// To get the number of elements in a vector, you can use the '.len()'
+// method like this: 'bank.accounts.len()'
+
+fn ex1_print_num_accounts(bank: &Bank) {
+    println!(
+        "Number of accounts in the bank: {}",
+        bank.accounts.len().green()
+    );
+}
+
+fn ex1() {
+    header("Exercise 1");
+
+    let mut bank = Bank::new();
+    let account1 = Account::new(1, String::from("me"));
+    let account2 = Account::new(1, String::from("me"));
+
+    bank.accounts.push(account1);
+    bank.accounts.push(account2);
+
+    // TODO: call 'print_num_accounts' here:
+    ex1_print_num_accounts(&bank);
+
+    // Notice the existing println statement here! 'main' needs to use the
+    // 'bank' value in two locations, so we probably shouldn't move the
+    // 'bank' value.
+    println!("{:#?}", bank);
 }
--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -2,15 +2,17 @@
 Main entry point for the Rust application.
 */
 
-mod conc;
+// mod conc;
 // mod sec3;
+mod exc;
 mod utils;
 
 // use sec3::bank1::bank1_main;
 // use conc::exc26::exc26_main;
 // use conc::own24::conc_main;
-use conc::bor31::bor31_main;
+// use conc::bor31::bor31_main;
+use exc::exc33::exc33_main;
 
 fn main() {
-    bor31_main();
+    exc33_main();
 }
